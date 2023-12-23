5248b2ec10bdc4683f2fd144bc476f756bf2071c
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2023-12-23 20:59:52 -0300

    Exercise 2 fix

---
+++ 2 Basic-1/Lasagna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Lasagna
+{
+    // 1. Define the expected oven time in minutes
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
+    // 2. Calculate the remaining oven time in minutes
+    public int RemainingMinutesInOven(int actualTime)
+    {
+        return ExpectedMinutesInOven() - actualTime;
+    }
+    // 3. Calculate the preparation time in minutes
+    public int PreparationTimeInMinutes(int numberLayers)
+    {
+        return 2 * numberLayers;
+    }
+    // 4. Calculate the elapsed time in minutes
+    public int ElapsedTimeInMinutes(int numberLayers, int actualTime)
+    {
+        return 2 * numberLayers + actualTime;
+    }
+}
---
+++ 2 Basic-1/Program.cs
@@ -0,0 +1,9 @@
+﻿var lasagna = new Lasagna();
+// 1.Define the expected oven time in minutes
+Console.WriteLine(lasagna.ExpectedMinutesInOven());
+// 2. Calculate the remaining oven time in minutes
+Console.WriteLine(lasagna.RemainingMinutesInOven(30));
+// 3. Calculate the preparation time in minutes
+Console.WriteLine(lasagna.PreparationTimeInMinutes(2));
+// 4. Calculate the elapsed time in minutes
+Console.WriteLine(lasagna.ElapsedTimeInMinutes(3, 20));
