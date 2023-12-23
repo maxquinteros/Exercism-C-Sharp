833aa85d5048fce0b5cfe8af60b3fc4f81d0140a
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2023-12-23 03:18:25 -0300

    Exercise 1 solution

--- 1-basico/Program.cs
+++ 1-basico/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _1_basico;
+
+var lasagna = new Lasagna();
+
+// Define the expected oven time in minutes
+Console.WriteLine(lasagna.ExpectedMinutesInOven());
+// Calculate the remaining oven time in minutes
+Console.WriteLine(lasagna.RemainingMinutesInOven(30));
+// Calculate the preparation time in minutes
+Console.WriteLine(lasagna.PreparationTimeInMinutes(2));
+// Calculate the elapsed time in minutes
+Console.WriteLine(lasagna.ElapsedTimeInMinutes(3, 20));
