fd1ed62b39f7ae58213369e9a1f1483a686196d7
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2023-12-25 03:44:04 -0300

    Exercise 3 solution

---
+++ 3 Booleans-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercise.QuestLogic;
+
+// Task 1: Check if a fast attack can be made
+var knightIsAwake = true;
+Console.WriteLine(QuestLogic.CanFastAttack(knightIsAwake));
+
+// Task 2: Check if the group can be spied upon
+knightIsAwake = false;
+var archerIsAwake = true;
+var prisonerIsAwake = false;
+Console.WriteLine(QuestLogic.CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake));
+
+// Task 3: Check if the prisioner can be signalled
+archerIsAwake = false;
+prisonerIsAwake = true;
+Console.WriteLine(QuestLogic.CanSignalPrisoner(archerIsAwake, prisonerIsAwake));
+
+// Task 4: Check if the prisoner can be freed
+knightIsAwake = false;
+archerIsAwake = true;
+prisonerIsAwake = false;
+var petDogIsPresent = false;
+Console.WriteLine(QuestLogic.CanFreePrisoner(knightIsAwake,archerIsAwake,prisonerIsAwake, petDogIsPresent));
---
+++ 3 Booleans-1/QuestLogic.cs
@@ -0,0 +1,73 @@
+﻿namespace Exercise.QuestLogic
+{
+    public static class QuestLogic
+    {
+        // Task 1: Check if a fast attack can be made
+        public static bool CanFastAttack(bool knightIsAwake)
+        {
+            if (knightIsAwake == true)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        // Task 2: Check if the group can be spied upon
+        public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+        {
+            int guardiansAwaken = 0;
+
+            guardiansAwaken += knightIsAwake == true ? 1 : 0;
+            guardiansAwaken += archerIsAwake == true ? 1 : 0;
+            guardiansAwaken += prisonerIsAwake == true ? 1 : 0;
+
+            return(guardiansAwaken != 0 ? true : false);
+        }
+
+        // Task 3: Check if the prisioner can be signalled
+        public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+        {
+            if (archerIsAwake == false && prisonerIsAwake == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Task 4: Check if the prisoner can be freed
+        public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+        {
+            if (knightIsAwake == false &&
+                archerIsAwake == false &&
+                prisonerIsAwake == false &&
+                petDogIsPresent == true)
+                return true;
+
+            else if (knightIsAwake == false &&
+                     archerIsAwake == false &&
+                     prisonerIsAwake == true)
+                return true;
+
+            else if (knightIsAwake == true &&
+                     archerIsAwake == false &&
+                     prisonerIsAwake == false &&
+                     petDogIsPresent == true)
+                return true;
+
+            else if (knightIsAwake == true &&
+                     archerIsAwake == false &&
+                     prisonerIsAwake == true &&
+                     petDogIsPresent == true)
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
