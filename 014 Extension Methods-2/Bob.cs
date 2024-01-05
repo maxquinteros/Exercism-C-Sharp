36c7e4df4a0823b322dfb2752846911736d2422a
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2024-01-05 01:42:29 -0300

    014 Solution

---
+++ 014 Extension Methods-2/Bob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Bob
+{
+    public static class Bob
+    {
+        public static string Response (string statement)
+        {
+            statement = statement.Trim ();
+            if (statement == "" || statement == null)
+            {
+                return "Fine. Be that way!";
+            }
+            else if (statement[^1] == '?')
+            {
+                return statement == statement.ToUpper() && statement.ToLower() != statement.ToUpper() ? "Calm down, I know what I'm doing!" : "Sure.";
+            }
+            else if (statement == statement.ToUpper() && statement.ToLower() != statement.ToUpper())
+            {
+                return "Whoa, chill out!";
+            }   
+            else
+            {
+                return "Whatever.";
+            }
+        }
+    }
+}
