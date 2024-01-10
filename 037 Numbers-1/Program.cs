cfd665ed0e04441035f50c7f5539166fa3fe5842
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2024-01-10 00:14:10 -0300

    Solution 037

---
+++ 037 Numbers-1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Exercism.AssemblyLine;
+
+// Task 1: Calculate the success rate
+Console.WriteLine(AssemblyLine.SuccessRate(10)); // => 0.77
+
+// Task 2: Calculate the production rate per hour
+Console.WriteLine(AssemblyLine.ProductionRatePerHour(6)); // => 1193.4
+
+// Task 3: Calculate the number of working items produced per minute
+Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(6)); // => 19
