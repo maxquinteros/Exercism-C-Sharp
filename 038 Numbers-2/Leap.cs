4373a6def18b15c96825e4bdcda3a59b2db1aa47
Maximiliano Quinteros Cortés <maximiliano.quinteros.cortes@hotmail.com>
2024-01-11 00:08:19 -0300

    Solution 038

---
+++ 038 Numbers-2/Leap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.Leap
+{
+    public static class Leap
+    {
+        public static bool IsLeapYear (int year)
+        {
+            if (year % 4 == 0 &&  year % 100 != 0 || year % 400 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
